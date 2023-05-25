cb35490d49b4c075bb505be0c170a5aa9234ae3c
Eduardo Rodrigues da Silva <edu0810@gmail.com>
2023-05-25 15:51:18 -0300

    Add script para desabilitar C

---
+++ Desabilitar Compartilhamento Padrão C$, D$, ADMIN/disable_c.ps1
@@ -0,0 +1,23 @@
+﻿########################## INFORMAÇOES ##########################################
+#
+#Nome do Script : disable_c.ps1
+#Descriçao      : Desabilitar Compartilhamento C$ do Windows. 
+#Autor          : Eduardo R. Silva
+#Canal Youtube  : https://youtu.be/-vbTs2AYFvk
+#Blog Acesso8   : https://acesso8.blogspot.com/2021/08/desabilitar-compartilhamento-c-do.html
+#versao         : 1.0
+#Complemento    : 
+#                  
+#
+#################################################################################
+
+# setar a politica de execucao de scripts
+Set-ExecutionPolicy Unrestricted -Force
+
+########################## CRIANDO CHAVE AutoShareWks ############################
+
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "AutoShareWks" -Value ”0”  -PropertyType "DWORD"
+
+########################### REINICAR COMPUTADOR LOCAL ############################
+
+#Restart-Computer
